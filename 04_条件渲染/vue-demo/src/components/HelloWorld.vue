949f66d75443435c730bc535a1eb7b5086614573
sectools007 <fant4sy.hx@gmail.com>
2022-10-14 12:47:27 +0800

    学习条件渲染

---
+++ 04_条件渲染/vue-demo/src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <p v-if="flag">我是孙猴子</p>
+    <p v-else>你是傻猴子</p>
+    <p  v-show="flag">你真的是那只孙猴子吗？</p>
+    <div v-if="flag">
+      <p>1</p>
+      <p>2</p>
+      <p>3</p>
+    </div>
+  </div>
+
+</template>
+  <!-- 
+    v-if/v-else:
+      用flag来给一个判断指令，可以对块进行控制
+    
+    v-show/v-if的区别：
+      v-if在dom结构中看不到，会将元素删除掉
+      v-show 用css控制，display none block的切换
+      
+      v-if有较高的切换开销
+      v-show you较高的渲染开销
+
+   -->
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      flag: true
+    }
+  }
+}
+</script>
